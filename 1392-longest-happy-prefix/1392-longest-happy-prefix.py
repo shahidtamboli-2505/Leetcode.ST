c5743dfaa5838603be3eac5592d53f77ab74b029
Shahid Tamboli <shahidboxer58@gmail.com>
2026-06-24 00:32:23 +0530

    Time: 163 ms (35.37%), Space: 19.2 MB (28.57%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPrefix(self, s):
+        n = len(s)
+
+        lps = [0] * n
+        j = 0
+
+        for i in range(1, n):
+            while j > 0 and s[i] != s[j]:
+                j = lps[j - 1]
+
+            if s[i] == s[j]:
+                j += 1
+                lps[i] = j
+
+        return s[:lps[-1]]
