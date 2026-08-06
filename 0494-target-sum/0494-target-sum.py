9ea4805414ca040c0b8df85562ad16327f66b2b2
Shahid Tamboli <shahidboxer58@gmail.com>
2026-08-06 23:40:31 +0530

    Time: 194 ms (12.01%), Space: 74.3 MB (14.4%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findTargetSumWays(self, nums, target):
+        memo = {}
+
+        def dfs(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+
+            if (i, total) in memo:
+                return memo[(i, total)]
+
+            add = dfs(i + 1, total + nums[i])
+            subtract = dfs(i + 1, total - nums[i])
+
+            memo[(i, total)] = add + subtract
+            return memo[(i, total)]
+
+        return dfs(0, 0)
